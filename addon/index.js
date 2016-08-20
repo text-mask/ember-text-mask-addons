8baf8d68c72ccf9b1ff3d158b24ae56f0dea1bb5
Loz Jackson <lozjackson@talktalk.net>
2016-08-20 16:08:59 +0100

    add lib

---
+++ addon/index.js
@@ -0,0 +1,9 @@
+import { autoCorrectedMmddyyyyPipe } from './lib/auto-corrected-mmddyyyy-pipe';
+import { createNumberMask } from './lib/create-number-mask';
+import { emailMask } from './lib/email-mask';
+
+export default {
+  autoCorrectedMmddyyyyPipe: autoCorrectedMmddyyyyPipe.default,
+  createNumberMask: createNumberMask.default,
+  emailMask: emailMask.default
+};
