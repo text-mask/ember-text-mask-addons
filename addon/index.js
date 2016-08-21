3b924b63633c6528dfec928f677a2e8c21642bb0
Loz Jackson <lozjackson@talktalk.net>
2016-08-21 23:05:32 +0100

    update index.js

--- addon/index.js
+++ addon/index.js
@@ -1,9 +1,12 @@
-import { autoCorrectedMmddyyyyPipe } from './lib/auto-corrected-mmddyyyy-pipe';
-import { createNumberMask } from './lib/create-number-mask';
-import { emailMask } from './lib/email-mask';
+import AutoCorrectedMmddyyyyPipe from './lib/auto-corrected-mmddyyyy-pipe';
+import CreateNumberMask from './lib/create-number-mask';
+import EmailMask from './lib/email-mask';
 
-export default {
-  autoCorrectedMmddyyyyPipe: autoCorrectedMmddyyyyPipe.default,
-  createNumberMask: createNumberMask.default,
-  emailMask: emailMask.default
-};
+const autoCorrectedMmddyyyyPipe = AutoCorrectedMmddyyyyPipe.autoCorrectedMmddyyyyPipe.default;
+const createNumberMask = CreateNumberMask.createNumberMask.default;
+const emailMask = EmailMask.emailMask.default;
+
+export default { autoCorrectedMmddyyyyPipe, createNumberMask, emailMask };
+export { autoCorrectedMmddyyyyPipe };
+export { createNumberMask };
+export { emailMask };
