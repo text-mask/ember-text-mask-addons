f3d1b453220f5fb146f5a4f38f05aac775eed17b
Loz Jackson <accounts@audiowall.co.uk>
2017-01-31 10:47:03 +0000

    Update mask computed properties (#15)
    * update auto-corrected-date-pipe
    
    * update number-mask component

--- addon/components/auto-corrected-date-pipe.js
+++ addon/components/auto-corrected-date-pipe.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import MaskedInputComponent from 'ember-text-mask/components/masked-input';
 import { createAutoCorrectedDatePipe } from 'ember-text-mask-addons';
 
+const { computed } = Ember;
 const dateFormatChars = ['d', 'm', 'y'];
 const defaultDateFormat = 'mm/dd/yyyy';
 
@@ -11,14 +12,16 @@ export default MaskedInputComponent.extend({
   keepCharPositions: true,
   placeholderChar: '\u2000',
 
+  createAutoCorrectedDatePipe,
+
   /*
     Return an object with both mask and pipe.
   */
-  mask: Ember.computed('dateFormat', function () {
+  mask: computed('dateFormat', function () {
     let dateFormat = this.get('dateFormat') || defaultDateFormat;
     return {
       mask: dateFormat.split('').map(char => (dateFormatChars.indexOf(char.toLowerCase()) === -1) ? char:/\d/),
-      pipe: createAutoCorrectedDatePipe(dateFormat)
+      pipe: this.createAutoCorrectedDatePipe(dateFormat)
     };
   })
 });
